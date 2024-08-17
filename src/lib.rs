a3453ee5e5ae1b032bb440c685bcda40b9fab397
ohmycloud <ohmycloudy@gmail.com>
2024-08-17 18:07:05 +0800

    auto deref with macro

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,12 @@
+mod auto_deref;
 mod enum_from;
 mod enum_from_darling;
 
+use crate::auto_deref::process_auto_deref;
 use crate::enum_from::process_enum_from;
 use crate::enum_from_darling::process_enum_from_darling;
 use proc_macro::TokenStream;
+use syn::DeriveInput;
 
 /// for enum, we'd like to generate From impls for each variant
 #[proc_macro_derive(EnumFrom)]
@@ -16,4 +19,11 @@ pub fn derive_enum_from(input: TokenStream) -> TokenStream {
 pub fn derive_enum_from_darling(input: TokenStream) -> TokenStream {
     let input = syn::parse_macro_input!(input as syn::DeriveInput);
     process_enum_from_darling(input).into()
+}
+
+#[proc_macro_derive(AutoDeref, attributes(deref))]
+pub fn derive_auto_deref(input: TokenStream) -> TokenStream {
+    let input = syn::parse_macro_input!(input as DeriveInput);
+    println!("{:#?}", input);
+    process_auto_deref(input).into()
 }
