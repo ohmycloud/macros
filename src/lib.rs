46e5cc0c3b44fcb14caa1aebfef9a3a021b364dc
ohmycloud <ohmycloudy@gmail.com>
2024-08-17 14:52:56 +0800

    simple proc macro

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,10 @@
-// my_vec! {1, 2, 3}
-#[macro_export]
-macro_rules! my_vec {
-    // 匹配空的 Vec: let v = my_vec![];
-    () => { Vec::new() };
-    // 匹配(元素; 元素个数): let v = my_vec![42; 5];
-    ($elem:expr; $n:expr) => {
-        std::vec::from_elem($elem, $n)
-    };
-    // 匹配一个或多个表达式或者逗号
-    ($($x:expr),+) => {
-        {
-            let mut temp_vec = Vec::new();
-            $(
-                temp_vec.push($x);
-            )+
-            temp_vec
-        }
-    };
-    // 匹配末尾可能的逗号
-    ($($x:expr),+ $(,)?) => {
-        {
-            <[_]>::into_vec(Box::new([$($x),*]))
-        }
-    };
+mod enum_from;
+use crate::enum_from::process_enum_from;
+use proc_macro::TokenStream;
+
+/// for enum, we'd like to generate From impls for each variant
+#[proc_macro_derive(EnumFrom)]
+pub fn derive_macro_derive(input: TokenStream) -> TokenStream {
+    let input = syn::parse_macro_input!(input as syn::DeriveInput);
+    process_enum_from(input).into()
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,1 @@
-use anyhow::Result;
-use macros::my_vec;
-
-fn main() -> Result<()> {
-    let v: Vec<i32> = my_vec!["193".parse()?, 21, 33, "42".parse()?, 57,];
-    println!("{:?}", v);
-
-    // 创建 Vec 的另外一种形式
-    let v1 = <[_]>::into_vec(Box::new([1, 2, 3, 4]));
-    println!("{:?}", v1);
-    Ok(())
-}
+fn main() {}
