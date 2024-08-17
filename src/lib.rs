c38361f8163dd2e206a3707316e8697b18f6344c
ohmycloud <ohmycloudy@gmail.com>
2024-08-17 16:59:56 +0800

    simple proc macro with darling

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,19 @@
 mod enum_from;
+mod enum_from_darling;
+
 use crate::enum_from::process_enum_from;
+use crate::enum_from_darling::process_enum_from_darling;
 use proc_macro::TokenStream;
 
 /// for enum, we'd like to generate From impls for each variant
 #[proc_macro_derive(EnumFrom)]
-pub fn derive_macro_derive(input: TokenStream) -> TokenStream {
+pub fn derive_enum_from(input: TokenStream) -> TokenStream {
     let input = syn::parse_macro_input!(input as syn::DeriveInput);
     process_enum_from(input).into()
+}
+
+#[proc_macro_derive(EnumFromDarling)]
+pub fn derive_enum_from_darling(input: TokenStream) -> TokenStream {
+    let input = syn::parse_macro_input!(input as syn::DeriveInput);
+    process_enum_from_darling(input).into()
 }
